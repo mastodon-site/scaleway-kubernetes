c375fe060ec6e7ee89c12cd66edf430d5f777be8
grrywlsn <6753180+grrywlsn@users.noreply.github.com>
2024-08-30 19:13:21 +0100

    Remove provider config

--- main.tf
+++ main.tf
@@ -1,15 +1,3 @@
-terraform {
-  backend "s3" {}
-  required_version = "> 1.0.0"
-}
-
-provider "scaleway" {
-  zone            = var.scaleway_zone
-  region          = var.scaleway_region
-  project_id      = var.scaleway_project_id
-  organization_id = var.scaleway_organization_id
-}
-
 provider "kubernetes" {
   host  = scaleway_k8s_cluster.cluster.kubeconfig[0].host
   token = scaleway_k8s_cluster.cluster.kubeconfig[0].token
