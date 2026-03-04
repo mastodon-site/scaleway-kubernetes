dc33feadcc0b4958c039835138a58b683d0a469f
garry <grrywlsn@users.noreply.github.com>
2026-03-04 09:46:53 +0000

    add ipv6

--- lb.tf
+++ lb.tf
@@ -1,10 +1,15 @@
-resource "scaleway_lb_ip" "ingress" {
+resource "scaleway_lb_ip" "ingress_ipv4" {
   count = var.create_lb ? 1 : 0
+}
+
+resource "scaleway_lb_ip" "ingress_ipv6" {
+  count   = var.create_lb ? 1 : 0
+  is_ipv6 = true
 }
 
 resource "scaleway_lb" "ingress" {
   count  = var.create_lb ? 1 : 0
-  ip_ids = [scaleway_lb_ip.ingress[0].id]
+  ip_ids = [scaleway_lb_ip.ingress_ipv4[0].id, scaleway_lb_ip.ingress_ipv6[0].id]
   name   = "${var.kubernetes_cluster_name}-ingress"
   type   = var.lb_type
 
--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,11 @@
-output "lb_ip_address" {
+output "lb_ipv4_address" {
   description = "Public IPv4 address of the ingress load balancer"
-  value       = var.create_lb ? scaleway_lb_ip.ingress[0].ip_address : null
+  value       = var.create_lb ? scaleway_lb_ip.ingress_ipv4[0].ip_address : null
+}
+
+output "lb_ipv6_address" {
+  description = "Public IPv6 address of the ingress load balancer"
+  value       = var.create_lb ? scaleway_lb_ip.ingress_ipv6[0].ip_address : null
 }
 
 output "lb_id" {
